8c196db5c5afabd2a89f6f72f8fb760233c267f8
diego velasco rivas <dvrcandrago@gmail.com>
2019-05-15 10:30:26 +0200

    add

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("hola mundo");
+const cd = [{
+        'name': 'asdas',
+        'artist': 'asdas',
+        'url': 'https://i.pinimg.com/originals/44/26/be/4426bedbd11289425446188827e96022.jpg',
+        'price': 23,
+        'desc':'lorem lorem'
+    },
+    {
+        'name': 'cd2',
+        'artist': 'asdas',
+        'url': 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/448319b4b1b43f8324abc31cee74b401.jpg?ts=1476426364',
+        'price': 26,
+        'desc':'lorem lorem'
+    }
+]
